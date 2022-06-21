f6240ebd8c9e5f61771673c1d22eacb5ce97ebf8
David Hofer-Zeni <d.hoferzeni@saa.at>
2022-06-21 16:11:54 +0200

    Changed port number for azure environment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT || 3000
+
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -21,6 +24,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
